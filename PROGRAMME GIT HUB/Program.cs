01eeafc8bf7f148300d01304eb4e5b57bc60a5b9
lchamprobert <lchamprobert@N112-04>
2024-09-19 16:05:46 +0200

    Ajoutez des fichiers projet.

---
+++ PROGRAMME GIT HUB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMME_GIT_HUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quel est votre pseudo ?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Votre pseudo est {0}", input);
+            Console.ReadKey();
+        }
+    }
+}
